de2b8ba68a364a384910f5653d8ecaea56dc16a9
Israel Ikier <israel.ikier@assai.com.br>
2021-11-20 17:51:17 -0300

    change message hellooooo

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "HELLO\n")
+	fmt.Fprintf(w, "HELLOOOOOO\n")
 }
 
 func headers(w http.ResponseWriter, req *http.Request) {
